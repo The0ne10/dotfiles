f9bc69c4033509cdfc17ba5f43706433920c291b
HomePC <siska@mail.ru>
2024-01-18 17:34:02 +0700

    change

--- nvim/nvim/nvim/lua/plugins/bufferline.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.termguicolors = true
-require("bufferline").setup{}
-vim.g.mapleader = " "
-
--- nnoremap <leader>gt :BufferLineCycleNext<CR>
--- nnoremap <leader>gT :BufferLineCyclePrev<CR>
-vim.keymap.set('n', 'gt',':BufferLineCycleNext<CR>')
-vim.keymap.set('n', 'gT',':BufferLineCyclePrev<CR>')
